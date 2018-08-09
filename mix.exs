2f7e1e442e2d3e46b4040b5a7f1f0df786e02e36
Boyd Multerer <boyd@multerer.com>
2018-08-09 15:47:36 -0700

    mark as v0.7.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Scenic.Driver.Glfw.MixProject do
   def project do
     [
       app: :scenic_driver_glfw,
-      version: "0.1.0",
+      version: "0.7.0",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
