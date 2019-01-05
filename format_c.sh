832bcfa72014c2195f385b601c066eaa2595f27a
Chris Ertel <>
2019-01-04 21:31:05 -0600

    Add reporting for linting and formatting if commands are missing.

--- format_c.sh
+++ format_c.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-clang-format -i -style=file c_src/*.*
+if command -v clang-format > /dev/null &2>1; then
+    clang-format -i -style=file c_src/*.*
+else
+    echo "You'll need to install clang-format (available via apt for Debian and derivatives) for this to work."
+    exit 1
+fi
--- lint_c.sh
+++ lint_c.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-cppcheck c_src/*.* --enable=all
+if command -v cppcheck > /dev/null &2>1; then
+    cppcheck c_src/*.* --enable=all
+else
+    echo "You'll need to install cppcheck (available via apt for Debian and derivatives) for this to work."
+    exit 1
+fi
